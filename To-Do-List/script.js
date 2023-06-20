7f7e4f0e10d95b21b0b436b59f3e6142f72dd7d9
Rohini Kulkarni <kulkarnirohini879@gmail.comgit config --global user.emailgit config --global user.email kulkarnirohini879@gmail.com>
2023-06-20 16:09:14 +0530

    completed
    done

--- To-Do-List/script.js
+++ To-Do-List/script.js
@@ -1,85 +1,296 @@
-var contador = 0
-,   select_opt = 0;
+var state = [];
 
-function add_to_list(){
-var action = document.querySelector('#action_select').value,
-description = document.querySelector('.input_description').value, 
-title = document.querySelector('.input_title_desc').value,
-date = document.getElementById('date_select').value;
- 
-
-switch (action) {
-  case "SHOPPING":
- select_opt  = 0;
-    break;
-case "WORK":
-select_opt = 1; 
-    break;
-case "SPORT":
- select_opt = 2;
-    break;
-case "MUSIC":
-select_opt = 3; 
-    break;
-}  
-  
-var class_li  =['list_shopping list_dsp_none','list_work list_dsp_none','list_sport list_dsp_none','list_music list_dsp_none'];  
-
-var cont = '<div class="col_md_1_list">    <p>'+action+'</p>    </div> <div class="col_md_2_list"> <h4>'+title+'</h4> <p>'+description+'</p> </div>    <div class="col_md_3_list"> <div class="cont_text_date"> <p>'+date+'</p>  </div>  <div class="cont_btns_options">    <ul>  <li><a href="#finish" onclick="finish_action('+select_opt+','+contador+');" ><i class="material-icons">&#xE5CA;</i></a></li>   </ul>  </div>    </div>';  
- 
-var li = document.createElement('li')  
-li.className = class_li[select_opt]+" li_num_"+contador;
-
-li.innerHTML = cont;
-document.querySelectorAll('.cont_princ_lists > ul')[0].appendChild(li);
-
-setTimeout(function(){  document.querySelector('.li_num_'+contador).style.display = "block";
-},100);
-  
-setTimeout(function(){
-  document.querySelector('.li_num_'+contador).className = "list_dsp_true "+class_li[select_opt]+" li_num_"+contador;
-contador++;
-},200);
-
+function setDefaultState() {
+  var id = generateID();
+  var baseState = {};
+  baseState[id] = {
+    status: "new",
+    id: id,
+    title: "This site uses 🍪to keep track of your tasks"
+  };
+  syncState(baseState);
 }
 
-function finish_action(num,num2) {
- 
-var class_li  =['list_shopping list_dsp_true','list_work  list_dsp_true','list_sport list_dsp_true','list_music list_dsp_true'];   
-console.log('.li_num_'+num2);
- document.querySelector('.li_num_'+num2).className = class_li[num]+" list_finish_state"; 
-setTimeout(function(){
-           del_finish();
-           },500);
+function generateID() {
+  var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+  return randLetter + Date.now();
 }
 
-function del_finish(){
-var li = document.querySelectorAll('.list_finish_state');
-    for(var e = 0; e < li.length; e++){
-/* li[e].style.left = "-100px"; */    
-li[e].style.opacity = "0";
-li[e].style.height = "0px";      
-li[e].style.margin = "0px";      
+function pushToState(title, status, id) {
+  var baseState = getState();
+  baseState[id] = { id: id, title: title, status: status };
+  syncState(baseState);
+}
+
+function setToDone(id) {
+  var baseState = getState();
+  if (baseState[id].status === 'new') {
+    baseState[id].status = 'done'
+  } else {
+    baseState[id].status =  'new';
+  }
+
+  syncState(baseState);
+}
+
+function deleteTodo(id) {
+  console.log(id)
+  var baseState = getState();
+  delete baseState[id]
+  syncState(baseState)
+}
+
+function resetState() {
+  localStorage.setItem("state", null);
+}
+
+function syncState(state) {
+  localStorage.setItem("state", JSON.stringify(state));
+}
+
+function getState() {
+  return JSON.parse(localStorage.getItem("state"));
+}
+
+function addItem(text, status, id, noUpdate) {
+  var id = id ? id : generateID();
+  var c = status === "done" ? "danger" : "";
+  var item =
+    '<li data-id="' +
+    id +
+    '" class="animated flipInX ' +
+    c +
+    '"><div class="checkbox"><span class="close"><i class="fa fa-times"></i></span><label><span class="checkbox-mask"></span><input type="checkbox" />' +
+    text +
+    "</label></div></li>";
+
+  var isError = $(".form-control").hasClass("hidden");
+
+  if (text === "") {
+    $(".err")
+      .removeClass("hidden")
+      .addClass("animated bounceIn");
+  } else {
+    $(".err").addClass("hidden");
+    $(".todo-list").append(item);
+  }
+
+  $(".refresh").removeClass("hidden");
+
+  $(".no-items").addClass("hidden");
+
+  $(".form-control")
+    .val("")
+    .attr("placeholder", "✍️ Add item...");
+  setTimeout(function() {
+    $(".todo-list li").removeClass("animated flipInX");
+  }, 500);
+
+  if (!noUpdate) {
+    pushToState(text, "new", id);
+  }
+}
+
+function refresh() {
+  $(".todo-list li").each(function(i) {
+    $(this)
+      .delay(70 * i)
+      .queue(function() {
+        $(this).addClass("animated bounceOutLeft");
+        $(this).dequeue();
+      });
+  });
+
+  setTimeout(function() {
+    $(".todo-list li").remove();
+    $(".no-items").removeClass("hidden");
+    $(".err").addClass("hidden");
+  }, 800);
+}
+
+$(function() {
+  var err = $(".err"),
+    formControl = $(".form-control"),
+    isError = formControl.hasClass("hidden");
+
+  if (!isError) {
+    formControl.blur(function() {
+      err.addClass("hidden");
+    });
+  }
+
+  $(".add-btn").on("click", function() {
+    var itemVal = $(".form-control").val();
+    addItem(itemVal);
+    formControl.focus();
+  });
+
+  $(".refresh").on("click", refresh);
+
+  $(".todo-list").on("click", 'input[type="checkbox"]', function() {
+    var li = $(this)
+      .parent()
+      .parent()
+      .parent();
+    li.toggleClass("danger");
+    li.toggleClass("animated flipInX");
+
+    setToDone(li.data().id);
+
+    setTimeout(function() {
+      li.removeClass("animated flipInX");
+    }, 500);
+  });
+
+  $(".todo-list").on("click", ".close", function() {
+    var box = $(this)
+      .parent()
+      .parent();
+
+    if ($(".todo-list li").length == 1) {
+      box.removeClass("animated flipInX").addClass("animated                bounceOutLeft");
+      setTimeout(function() {
+        box.remove();
+        $(".no-items").removeClass("hidden");
+        $(".refresh").addClass("hidden");
+      }, 500);
+    } else {
+      box.removeClass("animated flipInX").addClass("animated bounceOutLeft");
+      setTimeout(function() {
+        box.remove();
+      }, 500);
     }
 
-  setTimeout(function(){
-var li = document.querySelectorAll('.list_finish_state');
-    for(var e = 0; e < li.length; e++){
-  li[e].parentNode.removeChild(li[e]); 
-    }
-  },500);
-  
-  
-}
-var t = 0;
-function add_new(){  
-if(t % 2 == 0){  
- document.querySelector('.cont_crear_new').className = "cont_crear_new cont_crear_new_active";
+    deleteTodo(box.data().id)
+  });
 
-  document.querySelector('.cont_add_titulo_cont').className = "cont_add_titulo_cont cont_add_titulo_cont_active";
-  t++;
-}else {  document.querySelector('.cont_crear_new').className = "cont_crear_new";
-document.querySelector('.cont_add_titulo_cont').className = "cont_add_titulo_cont";  
-  t++;
-  } 
-}
+  $(".form-control").keypress(function(e) {
+    if (e.which == 13) {
+      var itemVal = $(".form-control").val();
+      addItem(itemVal);
+    }
+  });
+  $(".todo-list").sortable();
+  $(".todo-list").disableSelection();
+});
+
+var todayContainer = document.querySelector(".today");
+
+
+var d = new Date();
+
+
+var weekday = new Array(7);
+weekday[0] = "Sunday 🖖";
+weekday[1] = "Monday 💪😀";
+weekday[2] = "Tuesday 😜";
+weekday[3] = "Wednesday 😌☕️";
+weekday[4] = "Thursday 🤗";
+weekday[5] = "Friday 🍻";
+weekday[6] = "Saturday 😴";
+
+
+var n = weekday[d.getDay()];
+
+
+var randomWordArray = Array(
+  "Oh my, it's ",
+  "Whoop, it's ",
+  "Happy ",
+  "Seems it's ",
+  "Awesome, it's ",
+  "Have a nice ",
+  "Happy fabulous ",
+  "Enjoy your "
+);
+
+var randomWord =
+  randomWordArray[Math.floor(Math.random() * randomWordArray.length)];
+
+
+todayContainer.innerHTML = randomWord + n;
+
+$(document).ready(function() {
+  var state = getState();
+
+  if (!state) {
+    setDefaultState();
+    state = getState();
+  }
+
+  Object.keys(state).forEach(function(todoKey) {
+    var todo = state[todoKey];
+    addItem(todo.title, todo.status, todo.id, true);
+  });
+
+  var mins, secs, update;
+
+  init();
+  function init() {
+    (mins = 25), (secs = 59);
+  }
+
+
+  set();
+  function set() {
+    $(".mins").text(mins);
+  }
+
+
+  $("#start").on("click", start_timer);
+  $("#reset").on("click", reset);
+  $("#inc").on("click", inc);
+  $("#dec").on("click", dec);
+
+  function start_timer() {
+
+    set();
+
+    $(".dis").attr("disabled", true);
+
+    $(".mins").text(--mins);
+    $(".separator").text(":");
+    update_timer();
+
+    update = setInterval(update_timer, 1000);
+  }
+
+  function update_timer() {
+    $(".secs").text(secs);
+    --secs;
+    if (mins == 0 && secs < 0) {
+      reset();
+    } else if (secs < 0 && mins > 0) {
+      secs = 59;
+      --mins;
+      $(".mins").text(mins);
+    }
+  }
+
+
+  function reset() {
+    clearInterval(update);
+    $(".secs").text("");
+    $(".separator").text("");
+    init();
+    $(".mins").text(mins);
+    $(".dis").attr("disabled", false);
+  }
+
+
+  function inc() {
+    mins++;
+    $(".mins").text(mins);
+  }
+
+
+  function dec() {
+    if (mins > 1) {
+      mins--;
+      $(".mins").text(mins);
+    } else {
+      alert("This is the minimum limit.");
+    }
+  }
+}); 
+
